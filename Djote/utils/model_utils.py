fdcc7d65c16564477c45891c7b93e165bc101d10
soroor <soroor.mnz@gmail.com>
2024-04-19 17:15:40 +0330

    fix: add soft delete to authuser model

---
+++ Djote/utils/model_utils.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class SoftDeleteMixin(models.Model):
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
--- user/models.py
+++ user/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from Djote import settings
+from Djote.utils.model_utils import SoftDeleteMixin
 
 
-class AuthUser(AbstractUser):
+class AuthUser(AbstractUser, SoftDeleteMixin):
     class Meta:
         app_label = 'user'
