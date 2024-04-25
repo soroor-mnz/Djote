329f2225088053219a574275fd4f164b6cf3d986
soroor <soroor.mnz@gmail.com>
2024-04-25 19:10:32 +0330

    fix: settings files updates

--- Djote/settings.py
+++ Djote/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'Djote.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -132,3 +125,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
