afbe1bdb923dfaba7e6a5b6a7fba1584732db8eb
soroor <soroor.mnz@gmail.com>
2024-04-25 21:39:29 +0330

    fix: note crud

---
+++ main/api/views/views.py
@@ -0,0 +1,27 @@
+# Create your views here.
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+
+from Djote.utils.view_utils import ReadWriteSerializerViewMixin, StandardResultsSetPagination
+from main.filtersets import NoteFilterSet
+from main.models import Note
+from main.api.serializers.serializer import NoteReadSerializer, NoteWriteSerializer
+
+
+class NoteViewSet(viewsets.ModelViewSet, ReadWriteSerializerViewMixin):
+    read_serializer = NoteReadSerializer
+    write_serializer = NoteWriteSerializer
+    pagination_class = StandardResultsSetPagination
+    permission_classes = [IsAuthenticated]
+    filterset_class = NoteFilterSet
+    filter_backends = (SearchFilter, DjangoFilterBackend)
+    search_fields = ["title", "content"]
+
+    def get_queryset(self):
+        return Note.objects.all()
+
+    def get_object(self):
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get("pk"))
