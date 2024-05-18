13f1bb09981e291010245476a5b12023268dce48
soroor <soroor.mnz@gmail.com>
2024-05-18 14:02:31 +0330

    fix: resolve the failed test

--- main/tests/tests.py
+++ main/tests/tests.py
@@ -29,7 +29,7 @@ class ModelsTestCase(TestCase):
 
     def test_list_api(self):
         response = self.client.get(reverse('note-list'))
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data.get("count"), Note.objects.all().count())
 
     def test_list_api_filter_user(self):
